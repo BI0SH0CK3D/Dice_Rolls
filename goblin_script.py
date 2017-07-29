e420ad588f7a4c0770b766d0524a940ca0036915
BI0SH0CK3D <thadyplaysguitar@gmail.com>
2017-07-29 13:45:32 -0400

    Improved goblin kill script, generic monster class started

--- goblin_script.py
+++ goblin_script.py
@@ -1,15 +1,6 @@
-#python3
-"""
-Rough draft of a model to find any significance in damage output using a 1d8 vs. 2d4 weapon.
-I am will examine average damage, how often one does more damage than the other, how often they
-tie and number of hits required to kill various monsters.
-
-I will probably turn the rolls into a class called Roll for the hell of it, add PEP8 considerations 
-and
-"""
 import random
 
-
+# create a class of Goblins to kill
 class Goblin(object):
 
 	def __init__(self, name, max_health):
@@ -19,60 +10,62 @@ class Goblin(object):
 		self.current_health = self.max_health
 
 	def take_damage(self, damage):
+		
 		self.current_health -= damage
-		# print "%s takes %s damage, %s hp remaining" % (self.name, damage, self.current_health)
-		#if self.current_health <= 0:
-			# print "%s is dead" % self.name
 		return self.current_health
 
-def _2d4():
-	damage = random.randint(1,4) + random.randint(1,4)
-	#print "2d4:", damage
-	return int(damage)
+# simulate polyhedral dice with optional bonuses
+def roll(number, sides, bonus = 0):
 
-def _1d8():
-	damage = random.randint(1,8)
-	#print "1d8:", damage
-	return damage
+	number_rolled = 0
+	bonus_damage = number * bonus
+	for i in range(number):
+		number_rolled += random.randint(1, sides)
+	return number_rolled + bonus_damage
 
 # number of hits required to kill 1hd monster
-def kill_goblins(number_of_goblins, d8 = True):
+def hits_to_kill_goblins(number_of_goblins, d8 = True):
+	
 	data = []
+	# how many goblins to kill
 	number_of_goblins = int(number_of_goblins)
 	for i in range(number_of_goblins):
 		hits_to_kill = 0
-		victim = Goblin("Victim", random.randint(2,9))
+		# hp = 1d8+1
+		victim = Goblin("Victim", roll.randint(1,8,1))
 		while victim.current_health >= 1:
-			#print "HP", victim.current_health 
+			# use 1d8 or 2d4?
 			if d8 == True:
-				victim.take_damage(_1d8())
+				victim.take_damage(roll(1,8))
 				hits_to_kill += 1
 			elif d8 == False:
-				victim.take_damage(_2d4())
+				victim.take_damage(roll(2,4))
 				hits_to_kill += 1
-		#print hits_to_kill
+		# we want non decimals for later caldulations 2.9 hits is 2 hits
 		data.append(int(hits_to_kill))
 	return data
 
+# run two simulations of 100 baddies
+hits_per_kill_1d8 = hits_to_kill_goblins(100, d8 = True)
+hits_per_kill_2d4 = hits_to_kill_goblins(100, d8 = False)
 
-hits_per_kill_1d8 = kill_goblins(500000, d8 = True)
-hits_per_kill_2d4 = kill_goblins(500000, d8 = False)
 
-# print("1d8", sum(hits_per_kill_1d8)/len(hits_per_kill_1d8))
-# print("2d4", sum(hits_per_kill_2d4)/len(hits_per_kill_2d4))
-
-d8_dict = {}
-d4_dict = {}
+# make a dictonary of counts for each and display
+_1d8_dict = {}
+_2d4_dict = {}
 
 for item in hits_per_kill_1d8:
-	if item not in d8_dict:
-		d8_dict[item] = 0
-	d8_dict[item] += 1
+	if item not in _1d8_dict:
+		_1d8_dict[item] = 0
+	_1d8_dict[item] += 1
 
 for item in hits_per_kill_2d4:
-	if item not in d4_dict:
-		d4_dict[item] = 0
-	d4_dict[item] += 1
+	if item not in _2d4_dict:
+		_2d4_dict[item] = 0
+	_2d4_dict[item] += 1
 
-print d8_dict
-print d4_dict
+print("1d8 data:")
+print(_1d8_dict)
+print("2d4 data:")
+print(_2d4_dict)
+
---
+++ monster.py
@@ -0,0 +1,31 @@
+# create generic monster class outside of script
+import random 
+
+class Monster(object):
+
+	def __init__(self, name, max_health):
+
+		self.name = name
+		self.max_health = max_health
+		self.current_health = self.max_health
+
+	def take_damage(self, amount):
+
+		self.current_health -= amount
+		return self.current_health
+
+
+# simulate polyhedral dice with optional bonuses
+def roll(number, sides, bonus = 0):
+
+	number_rolled = 0
+	bonus_damage = number * bonus
+	for i in range(number):
+		number_rolled += random.randint(1, sides)
+	return number_rolled + bonus_damage
+
+print(roll(1,20,4))
+
+hodor = Monster("Hodor", 15)
+hodor.take_damage(roll(1,12))
+print hodor.current_health
